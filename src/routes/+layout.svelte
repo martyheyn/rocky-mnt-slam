de5b7ad5dfb84353d41dccf81bb5a59f61fc08d4
Marty Heyn <marty@techslice.com>
2023-11-15 18:05:29 -0500

    added vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
 	import '../app.css';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	// Inject the Analytics functionality
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head>
